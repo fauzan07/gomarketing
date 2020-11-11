d7198d24d330a70997dd5a8d1871dbcf703c94c4
WWS <fauzanfalke@gmail.com>
2020-11-11 18:04:11 +0530

    addfiles

---
+++ header.php
@@ -0,0 +1,50 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>GoMarketing</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <link rel="shortcut icon" type="image/x-icon" href="#">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Fav Icon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,400i,500,500i,600,600i,700,900&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <!-- header start -->
+    <header id="header-wrap">
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+            <div class="container-fluid">
+                <a href="index.php" class="navbar-brand"><img src="images/logo.png" width="250" alt="Responsive image"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item pr-4"><a class="nav-link" href="#">home</a></li>
+                        <li class="nav-item pr-4"><a class="nav-link" href="#">services</a></li>
+                        <li class="nav-item pr-4"><a class="nav-link" href="#">industries</a></li>
+                        <li class="nav-item pr-4"><a class="nav-link" href="#">our methodology</a></li>
+                        <li class="nav-item pr-4"><a class="nav-link" href="#">clients</a></li>
+                        <li class="nav-item pr-4"><a class="nav-link" href="#">blog</a></li>
+                        <li class="nav-item pr-4"><a class="nav-link" href="#">about us</a></li>
+                        <li class="nav-item pr-4"><a class="nav-link" href="#">contact us</a></li>
+                    </ul>
+                </div>
+             
+            </div>
+        </nav>
+    </header>
+    <!-- header end -->
---
+++ index.php
@@ -0,0 +1,265 @@
+  <?php  include('header.php'); ?>
+
+  <!-- banners section start -->
+  <section class="banner-section">
+      <video class="device-watch-video img-fluid" autoplay muted>
+          <source src="images/digital-marketing-agency.mpeg" type="video/mp4">
+      </video>
+  </section>
+  <!-- banners section ends -->
+
+  <section class="avoid-law">
+    <div class=".container-fluid-about">
+      <div class="row">
+        <div class="col-lg-3">
+        <img src="images/banner-img1.png" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-6">
+          <p class="Lawsuits-text text-center pt-2">Avoid Lawsuits, Get Your Website ADA Compliant! <a href="#" class="learn-more">Learn More</a></p>
+        </div>
+        <div class="col-lg-3">
+        <img src="images/banner-img2.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="logo-slider py-5">
+  <div class="container-fluid">
+  <h4 class="text-center text-secondary pb-5">A Few Companies We help grow</h4>
+    <div class="row">
+      <div class="col-lg-12">
+        <img src="images/gomarketing-logo-bar.jpg" class="img-fluid" alt="">
+      </div>
+    </div>
+  </div>
+  </section>
+
+  <section class="about py-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5">
+          <h2>GoMarketing is Your Only Digital Marketing Solution For <b>Game Changing</b> <span class="text-danger"><b>Online Growth</b><span></h2>
+          <small class="text-secondary">Full-Service Digital Marketing Agency</small>
+          <hr>
+          <p>It’s time to get serious about your growth. Since 2008, GoMarketing has built professional, results driven, online marketing programs, designed to convert visitors to sales. We utilize the latest SEO, Link Building, PPC programs, and Social Media marketing strategies to build your online presence, drive visitors to your site, and generate new sales conversions.</p>
+          <p>Does it work? Absolutely! GoMarketing is the award-winning design, marketing and advertising agency that will help you to realize your business sales goals. Let us build you a market leading website, expose it with the latest search engine marketing techniques and social media marketing programs. We will learn to Understand your Goals, Engage our Professional Marketers, Succeed in our Efforts, then continually Grow Your Business.</p>
+        </div>
+        <div class="col-lg-7">
+          <div class="about-img">
+          <img src="images/about.png" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+  <section class="services py-2">
+    <div class="container-fluid">
+      <h2 class="text-center py-5">our services</h2>
+      <div class="row justify-content-center">
+        <div class="col-lg-2 text-center">
+          <div class="icon1">
+              <i class="fab fa-searchengin"></i>
+          </div>
+          <h4 class="service-text">Search Engine Optimization</h4>
+          <p>Improve your website ranking with our search engine optimization services</p>
+        </div>
+        <div class="col-lg-2 text-center">
+           <div class="icon1">
+              <i class="fas fa-chart-line"></i>
+          </div>
+          <h4 class="service-text">Search Engine Optimization</h4>
+          <p>A digital marketing company for small business owners</p>
+        </div>
+        <div class="col-lg-2 text-center">
+           <div class="icon1">
+               <i class="fas fa-share-alt"></i>
+          </div>
+          <h4 class="service-text">Social Media Marketing</h4>
+          <p>Increase Online presence using our Social Media Marketing</p>
+        </div>
+        <div class="col-lg-2 text-center">
+           <div class="icon1">
+               <i class="fas fa-money-bill-wave"></i>
+          </div>
+          <h4 class="service-text">Pay-Per-Click</h4>
+          <p>Pay-per-click advertising campaign to generate online sales</p>
+        </div>
+        <div class="col-lg-2 text-center">
+           <div class="icon1">
+              <i class="fas fa-laptop-code"></i>
+          </div>
+          <h4 class="service-text">Website Design</h4>
+          <p>Beautiful & well-crafted web designs that feature a responsive website</p>
+        </div>
+      </div>
+      <div class="row justify-content-center pt-4">
+        <div class="col-lg-2 text-center">
+           <div class="icon1">
+               <i class="fab fa-searchengin"></i>
+          </div>
+          <h4 class="service-text">Search Engine Optimization</h4>
+          <p>Improve your website ranking with our search engine optimization services</p>
+        </div>
+        <div class="col-lg-2 text-center">
+           <div class="icon1">
+               <i class="fas fa-chart-line"></i>
+          </div>
+          <h4 class="service-text">Search Engine Optimization</h4>
+          <p>A digital marketing company for small business owners</p>
+        </div>
+        <div class="col-lg-2 text-center">
+           <div class="icon1">
+                 <i class="fas fa-share-alt"></i>
+          </div>
+          <h4 class="service-text">Social Media Marketing</h4>
+          <p>Increase Online presence using our Social Media Marketing</p>
+        </div>
+        <div class="col-lg-2 text-center">
+           <div class="icon1">
+               <i class="fas fa-money-bill-wave"></i>
+          </div>
+          <h4 class="service-text">Pay-Per-Click</h4>
+          <p>Pay-per-click advertising campaign to generate online sales</p>
+        </div>
+        <div class="col-lg-2 text-center">
+           <div class="icon1">
+               <i class="fas fa-laptop-code"></i>
+          </div>
+          <h4 class="service-text">Website Design</h4>
+          <p>Beautiful & well-crafted web designs that feature a responsive website</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="internet-marketing py-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5">
+          <h2>Our Internet Marketing (SEO) <b>Strategies for</b> <span class="text-danger"><b>Your Successh</b><span></h2>
+          <hr>
+          <p>GoMarketing utilizes over a dozen marketing strategies for our Clients. Which ones we use for you and your business, when we use them, and how much we use them on your campaigns, displays the true art and expertise of GoMarketing.</p>
+          <p>Some companies will benefit greatly with Dominant Local Business Marketing, but other national or international companies will not. Some industries flourish online with Geo-Targeted pages, while others gain their best traction with Product pages. Directory linking works great with service industries, while professional relationship-based companies do better with expanded relative content and dialogue.</p>
+          <p>GoMarketing has worked with hundreds of Clients in all industries and professions. Over time, we have learned a lot more than the basic rules of SEO marketing. Consider SEO guides undergraduate work, simply the foundation of SEO. GoMarketing has a PhD in SEO internet marketing. Let us put it to work for you.</p>
+        </div>
+        <div class="col-lg-7">
+          <div class="about-img">
+          <img src="images/SEO-strategies.jpg" class="img-fluid" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="insider py-5">
+    <div class="container">
+    <h2 class="text-center pb-5">Gomarketing Growth Insider</h2>
+      <div class="row">
+        <div class="col-lg-4">
+          <img src="images/plumbing_digital_marketing.jpg" class="img-fluid" alt="insider-img">
+          <h3 class="insider-text">Top 5 Online Marketing Tips for Plumbers</h3>
+        </div>
+        <div class="col-lg-4">
+          <img src="images/gomarketing_digital_marketing.jpg" class="img-fluid" alt="insider-img">
+          <h3 class="insider-text">7 Digital Marketing Strategies During and After CoronavirusTop 5 Online Marketing Tips for Plumbers</h3>
+        </div>
+        <div class="col-lg-4">
+          <img src="images/survival_guide_for_restaurants.jpg" class="img-fluid" alt="insider-img">
+          <h3 class="insider-text">Survival Guide for Restaurants During Corona Virus Closures!</h3>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <img src="images/ada_website_compliance.jpg" class="img-fluid" alt="insider-img">
+          <h3 class="insider-text">The Small Business Guide To Making Your Website ADA Compliant + Accessible</h3>
+        </div>
+        <div class="col-lg-4">
+          <img src="images/ADA-Compliance.jpg" class="img-fluid" alt="insider-img">
+          <h3 class="insider-text">GoMarketing’s CEO Richard Uzelac, Announces New Americans with Disabilities Act Website Compliance Services</h3>
+        </div>
+        <div class="col-lg-4">
+          <img src="images/sued_industries.jpg" class="img-fluid" alt="insider-img">
+          <h3 class="insider-text">14 Most Sued Industries for Americans with Disabilities Act (ADA) Non-Compliance</h3>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <img src="images/small-business.jpg" class="img-fluid" alt="insider-img">
+          <h3 class="insider-text">The Guide To Marketing Your Small Business On A Budget</h3>
+        </div>
+        <div class="col-lg-4">
+          <img src="images/your-small-business.jpg" class="img-fluid" alt="insider-img">
+          <h3 class="insider-text">How To Market Your Small Business During Slower Seasons</h3>
+        </div>
+        <div class="col-lg-4">
+          <img src="images/google-bert-algorithm.jpg" class="img-fluid" alt="insider-img">
+          <h3 class="insider-text">What are the Google BERT Algorithm Changes? Explained by Richard Uzelac, CEO of GoMarketing</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="why-choose py-5">
+    <div class="container">
+      <h1 class="text-center py-3">Why Choose our <b>Digital Marketing Company <span class="text-danger">over Others?</span></b></h1>
+      <p class="text-center pb-4">You are searching, you have a need, you or your company has some pain, or you just want your freight train of success to keep on rolling faster and faster. GoMarketing is here to understand your needs, engage with your goals, make sure you succeed and then work with you to grow your business beyond your expectations.</p>
+      <div class="row">
+        <div class="col-lg-6">
+            <div class="border shadow p-4 text-center">
+                <div class="icon2">
+                <i class="fab fa-soundcloud"></i>
+                </div>
+                <h4 class="service-text pb-2">Understand</h4>
+                <small class="text-danger">“When people talk, listen completely. Most people never listen.” ― Ernest Hemingway</small>
+                <p class="pt-4">We start with a discussion. Where is your business now? Where do you want it in five years? What are your services or products or both? What are your business goals? What are your strengths? What are your weaknesses? Who are your Customers or Clients? What do they need from you? Who is your competition? What makes you different from your competition? What is your sales cycle? How do you want to engage clients? By walk-in, phone, chat, email, forms?</p>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="border shadow p-4 text-center">
+                <div class="icon2">
+                    <i class="fas fa-share-square"></i>
+                </div>
+                <h4 class="service-text pb-2">Engaged</h4>
+                <small class="text-danger">“When people talk, listen completely. Most people never listen.” ― Ernest Hemingway</small>
+                <p class="pt-4">We start with a discussion. Where is your business now? Where do you want it in five years? What are your services or products or both? What are your business goals? What are your strengths? What are your weaknesses? Who are your Customers or Clients? What do they need from you? Who is your competition? What makes you different from your competition? What is your sales cycle? How do you want to engage clients? By walk-in, phone, chat, email, forms?</p>
+            </div>
+        </div>
+      </div>
+
+      <div class="row pt-4">
+        <div class="col-lg-6">
+            <div class="border shadow p-4 text-center">
+                <div class="icon2">
+                <i class="far fa-thumbs-up"></i>
+                </div>
+                <h4 class="service-text pb-2">Success</h4>
+                <small class="text-danger">“When people talk, listen completely. Most people never listen.” ― Ernest Hemingway</small>
+                <p class="pt-4">We start with a discussion. Where is your business now? Where do you want it in five years? What are your services or products or both? What are your business goals? What are your strengths? What are your weaknesses? Who are your Customers or Clients? What do they need from you? Who is your competition? What makes you different from your competition? What is your sales cycle? How do you want to engage clients? By walk-in, phone, chat, email, forms?</p>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="border shadow p-4 text-center">
+                <div class="icon2">
+                    <i class="fas fa-chart-line"></i>
+                </div>
+                <h4 class="service-text pb-2">Grow</h4>
+                <small class="text-danger">“When people talk, listen completely. Most people never listen.” ― Ernest Hemingway</small>
+                <p class="pt-4">We start with a discussion. Where is your business now? Where do you want it in five years? What are your services or products or both? What are your business goals? What are your strengths? What are your weaknesses? Who are your Customers or Clients? What do they need from you? Who is your competition? What makes you different from your competition? What is your sales cycle? How do you want to engage clients? By walk-in, phone, chat, email, forms?</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  
+
+
+  <?php include('footer.php'); ?>
